34ef48c82c90dd7d7bc609c52e75afca37b1a964
hod0ri <minthod0ri@gmail.com>
2023-01-05 19:25:11 +0900

    [ Dev ] Register Unit Test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.views import status
 
-# Create your tests here.
+from django.shortcuts import resolve_url
+from django.urls import reverse
+from .models import *
+
+
+class UserAPITestCase(APITestCase):
+    def setUp(self):
+        self.reg_url = '/api/register'
+        self.reg_login = '/api/login'
+        self.regData = {
+            "name": "user1",
+            "email": "user1@gmail.com",
+            "password": "qwer1234!"
+        }
+
+    def test_register(self):
+        response = self.client.post(self.reg_url, data=self.regData, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
